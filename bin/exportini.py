0957f3d1fcf77039af5e91936ff1237d1461875b
Dmitry Rodin <madiedinro@gmail.com>
2018-07-22 21:36:29 +0300

    try export config using python

---
+++ bin/exportini.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import configparser
+import sys
+
+section = 'general'
+
+if len(sys.argv) == 2:
+    fn = sys.argv[1]
+
+    config = configparser.ConfigParser()
+    config.read(fn)
+
+    if 'general' in config:
+        for key in config[section]:  
+            print(f'export {key.upper()}="{config[section][key]}"')
